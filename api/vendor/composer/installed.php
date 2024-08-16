7b98eea4fc03996a61a7efa717b89d239836f9ce
giosurmanidze <giorgisurmanidze917@gmail.com>
2024-08-16 13:05:07 +0400

    feat/composer package

---
+++ api/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'giorgi/api',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd7c445bc8b2c601020aaf78cc2acbe8e7bfcbc2c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'giorgi/api' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd7c445bc8b2c601020aaf78cc2acbe8e7bfcbc2c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

28094381d001d1795f386659582792f29940680a
giosurmanidze <giorgisurmanidze917@gmail.com>
2024-08-16 13:38:52 +0400

    feat(models)/methods and properties to abstractProduct

--- api/app/models/AbstractProduct.php
+++ api/app/models/AbstractProduct.php
@@ -2,4 +2,16 @@
 
 namespace App\Models;
 
-class AbstractProduct {}
+abstract class AbstractProduct
+{
+    protected function __construct(
+        protected string $sku,
+        protected string $name,
+        protected float $price,
+        protected string $type,
+    ) {}
+
+    abstract protected function save(): void;
+    abstract protected function getData(): array;
+    abstract protected static function getAll(string $dbTable): ?array;
+}

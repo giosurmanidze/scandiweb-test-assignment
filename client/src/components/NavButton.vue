b2dce2923bbb58fd311c1c577870164d1e33e2ac
giosurmanidze <giorgisurmanidze917@gmail.com>
2024-08-19 21:50:38 +0400

    style: custom buttons and header

---
+++ client/src/components/NavButton.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  pageName: String,
+  btnContent: String
+})
+</script>
+
+<template>
+  <RouterLink :to="{ name: pageName }" class="relative">
+    <span class="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-black"></span>
+    <span
+      class="fold-bold relative inline-block h-full w-full rounded border-2 border-black bg-white px-5 sm:py-1 md:py-2 text-base font-bold text-black transition duration-100 hover:bg-[#e04f4f] hover:text-gray-900"
+      >{{ btnContent }}</span
+    >
+  </RouterLink>
+</template>
---
+++ client/src/components/SubmitButton.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  btnContent: {
+    type: String
+  }
+})
+</script>
+
+<template>
+  <button type="submit" class="relative">
+    <span class="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-gray-700"></span>
+    <span
+      class="fold-bold relative inline-block h-full w-full rounded border-2 border-black bg-black px-3 py-1 text-base font-bold text-white transition duration-100 hover:bg-gray-900 hover:text-[#e04f4f]"
+      >{{ btnContent }}</span
+    >
+  </button>
+</template>

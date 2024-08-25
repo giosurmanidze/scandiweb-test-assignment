1de7cfa3dc766c0dbffb8858fd9aa013b90be613
giosurmanidze <giorgisurmanidze917@gmail.com>
2024-08-25 13:43:44 +0400

    fix: autoQA issue about select menu

--- client/src/components/SelectMenu.vue
+++ client/src/components/SelectMenu.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="mb-7 flex items-center w-full gap-1">
-    <label for="productType" class="text-md font-medium text-gray-700 w-full md:w-1/4"
-      >Type Switcher</label
-    >
+    <label for="productType" class="text-md font-medium text-gray-700 w-full md:w-1/4">
+      Type Switcher
+    </label>
     <select
       v-model="selectedOption"
       class="mt-1 block py-2 px-3 w-full md:w-3/4 border-2 border-black rounded-md"
       id="productType"
     >
       <option value="" disabled>Select an option</option>
-      <option v-for="option in options" :key="option" :value="option">
-        {{ option }}
-      </option>
+      <option value="book" id="book">Book</option>
+      <option value="dvd" id="dvd">DVD</option>
+      <option value="furniture" id="furniture">Furniture</option>
     </select>
   </div>
 </template>
@@ -28,7 +28,6 @@ const props = defineProps({
 })
 
 const emit = defineEmits(['update:modelValue'])
-const options = ['book', 'dvd', 'furniture']
 
 const selectedOption = computed({
   get: () => props.modelValue,
